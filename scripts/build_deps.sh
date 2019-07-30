10c5badc5b2d9bb2c38de6b21a7a46ffa7b3d79c
Ahmed Yarub Hani Al Nuaimi <ahmedyarub@yahoo.com>
2019-07-30 15:26:27 -0300

    Travis configuration

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -4,16 +4,16 @@ JOBS=${JOBS:-`cat /proc/cpuinfo | grep processor | wc -l`}
 mkdir -p built_deps
 mkdir -p deps
 
-rm /usr/bin/gcc
-ln -s /usr/bin/gcc-9 /usr/bin/gcc
-rm /usr/bin/g++
-ln -s /usr/bin/g++-9 /usr/bin/g++
+sudo rm /usr/bin/gcc
+sudo ln -s /usr/bin/gcc-9 /usr/bin/gcc
+sudo rm /usr/bin/g++
+sudo ln -s /usr/bin/g++-9 /usr/bin/g++
 
 if [ ! -f "/usr/local/bin/flyway" ]
 then
     cd deps
     wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.2.4/flyway-commandline-5.2.4-linux-x64.tar.gz | tar xvz
-    ln -s $(pwd)/flyway-5.2.4/flyway /usr/local/bin
+    sudo ln -s $(pwd)/flyway-5.2.4/flyway /usr/local/bin
     cd ..
 fi
 flyway -locations=filesystem:./db/migrations/sql/ -url=jdbc:sqlite:/tmp/microservice.db -user= -password=  migrate
@@ -23,7 +23,7 @@ then
     cd deps
     wget https://cmake.org/files/v3.14/cmake-3.14.5-Linux-x86_64.sh
     sh cmake-3.14.5-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
-    ln -s /opt/cmake/bin/cmake /usr/bin/cmake
+    sudo ln -s /opt/cmake/bin/cmake /usr/bin/cmake
 fi
 cd ../
 
