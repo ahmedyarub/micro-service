93fb04c9bbd059647f4a1bc824c30debdc930e0d
Ahmed Yarub Hani Al Nuaimi <ahmedyarub@yahoo.com>
2019-07-30 15:24:02 -0300

    Travis configuration

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -9,28 +9,40 @@ ln -s /usr/bin/gcc-9 /usr/bin/gcc
 rm /usr/bin/g++
 ln -s /usr/bin/g++-9 /usr/bin/g++
 
-if [ ! -f "/usr/local/bin/flyway" ]; then
+if [ ! -f "/usr/local/bin/flyway" ]
+then
     cd deps
     wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.2.4/flyway-commandline-5.2.4-linux-x64.tar.gz | tar xvz
-    ln -s `pwd`/flyway-5.2.4/flyway /usr/local/bin
+    ln -s $(pwd)/flyway-5.2.4/flyway /usr/local/bin
     cd ..
 fi
 flyway -locations=filesystem:./db/migrations/sql/ -url=jdbc:sqlite:/tmp/microservice.db -user= -password=  migrate
 
-if [ ! -d "built_deps/vcpkg" ]; then
+if [ ! -f "/opt/cmake" ]
+then
+    cd deps
+    wget https://cmake.org/files/v3.14/cmake-3.14.5-Linux-x86_64.sh
+    sh cmake-3.14.5-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
+    ln -s /opt/cmake/bin/cmake /usr/bin/cmake
+fi
+cd ../
+
+if [ ! -d "built_deps/vcpkg" ]
+then
     cd deps
     git clone git://github.com/microsoft/vcpkg.git
     cd vcpkg
     ./bootstrap-vcpkg.sh
     export VCPKG_TRIPLET=x64-linux && \
-    echo '\nset(VCPKG_BUILD_TYPE release)' >> ./triplets/${VCPKG_TRIPLET}.cmake && \
+    echo '\nset(VCPKG_BUILD_TYPE release)' >> ./triplets/${VCPKG_TRIPLET}.cmake
 else
     cd vcpkg
 fi
 ./vcpkg install http-parser fmt restinio clara cpp-redis json-dto SQLite3 boost-lexical-cast boost-math
 cd ../../
 
-if [ ! -d "built_deps/soci" ]; then
+if [ ! -d "built_deps/soci" ]
+then
     cd deps
     git clone git://github.com/SOCI/soci.git
     cd ../built_deps
