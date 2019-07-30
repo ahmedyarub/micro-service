e6ea983051797ec3c641eab4e38819cd6dfd2286
Ahmed Yarub Hani Al Nuaimi <ahmedyarub@yahoo.com>
2019-07-30 17:08:06 -0300

    Travis configuration

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -4,13 +4,13 @@ JOBS=${JOBS:-`cat /proc/cpuinfo | grep processor | wc -l`}
 mkdir -p built_deps
 mkdir -p deps
 
-if [ ! -f "build_deps/flyway-5.2.4/flyway" ]
+if [ ! -f "${TRAVIS_BUILD_DIR}/build_deps/flyway-5.2.4/flyway" ]
 then
     cd built_deps
     wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.2.4/flyway-commandline-5.2.4-linux-x64.tar.gz | tar xvz
     cd "${TRAVIS_BUILD_DIR}"
 fi
-sudo ln -s $(pwd)/built_deps/flyway-5.2.4/flyway /usr/local/bin
+sudo ln -s ${TRAVIS_BUILD_DIR}/built_deps/flyway-5.2.4/flyway /usr/local/bin
 flyway -locations=filesystem:./db/migrations/sql/ -url=jdbc:sqlite:/tmp/microservice.db -user= -password=  migrate
 
 if [ ! -d "built_deps/vcpkg" ]
@@ -27,11 +27,11 @@ fi
 
 if [ ! -d "built_deps/soci" ]
 then
-    cd deps
+    cd ${TRAVIS_BUILD_DIR}/deps
     git clone git://github.com/SOCI/soci.git
-    cd ../built_deps
+    cd ${TRAVIS_BUILD_DIR}/built_deps
     mkdir -p soci && cd soci
-    cmake -DCMAKE_TOOLCHAIN_FILE=${TRAVIS_BUILD_DIR}/built_deps/vcpkg/scripts/buildsystems/vcpkg.cmake -DWITH_SQLITE3=ON -DWITH_MYSQL=OFF -DSOCI_TESTS=OFF -DCMAKE_INSTALL_PREFIX:PATH=/usr ../../deps/soci/
+    cmake -DCMAKE_TOOLCHAIN_FILE=${TRAVIS_BUILD_DIR}/built_deps/vcpkg/scripts/buildsystems/vcpkg.cmake -DWITH_SQLITE3=ON -DWITH_MYSQL=OFF -DSOCI_TESTS=OFF -DCMAKE_INSTALL_PREFIX:PATH=/usr ${TRAVIS_BUILD_DIR}/deps/soci/
     make -j${JOBS}
 else
     cd built_deps/soci/
